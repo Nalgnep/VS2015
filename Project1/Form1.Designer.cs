6740285581c330edf4b9cb4f72853bb66c8ffcce
Dorado El <chenhanbin@outlook.com>
2017-03-01 14:03:05 +0800

    123

--- Project1/Form1.Designer.cs
+++ Project1/Form1.Designer.cs
@@ -120,7 +120,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(195, 158);
+            this.button4.Location = new System.Drawing.Point(195, 178);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 8;
